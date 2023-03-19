7c1732c246524e20832789312277c8ebd08dd8e4
@karzelguizio <kawrantin.arzel--guiziou@etudiant.univ-rennes1.fr>
2023-03-19 12:16:45 +0100

    tests pour l'objet Analyse

---
+++ src/test/scala/analyse_phrase/TestAnalyse.scala
@@ -0,0 +1,59 @@
+package analyse_phrase
+import org.junit.Test
+import org.junit.Assert._
+
+class TestAnalyse {
+
+    // tests unitaires fonction "decouper"
+
+    @Test
+    def test_decouper_1 {
+        assertEquals(
+            List(""),
+            AnalyseImpl.decouper("")
+        )
+    }
+
+    @Test
+    def test_decouper_2 {
+        assertEquals(
+            List("hello"),
+            AnalyseImpl.decouper("hello")
+        )
+    }
+
+    @Test
+    def test_decouper_3 {
+        assertEquals(
+            List("où","est","la","mairie","?"),
+            AnalyseImpl.decouper("où est la mairie ?")
+        )
+    }
+
+    // tests unitaires fonction "analyser"
+
+    @Test
+    def test_analyser_1 {
+        assertEquals(
+            List(),
+            AnalyseImpl.analyser(List("où","est","la","mairie","?"))
+        )
+    }
+
+    @Test
+    def test_analyser_2 {
+        assertEquals(
+            List(("Mairie de Rennes","Place de la Mairie")),
+            AnalyseImpl.analyser(List("où","est","la","mairie","?"))
+        )
+    }
+
+    @Test
+    def test_analyser_3 {
+        assertEquals(
+            List(("Mairie de Rennes","Place de la Mairie"), ("Gare SNCF", "Place de la Gare")),
+            AnalyseImpl.analyser(List("où","est","la","mairie","et","la","gare","?"))
+        )
+    }
+
+}
