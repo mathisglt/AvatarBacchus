a63a47b15adf5f5d10bec51f4d9efde2d8d40581
Evan <evan.froc35@gmail.com>
2023-03-19 17:31:31 +0100

    ajout v0 ConstructionImpl

--- src/main/scala/construction_result/ConstructionImpl.scala
+++ src/main/scala/construction_result/ConstructionImpl.scala
@@ -2,4 +2,14 @@ package construction_result
 
 class ConstructionImpl extends ConstructionTrait{
   
+    val resultat = AnalyseImpl.analyser
+
+    /**
+      * Construit une phrase propre à partir du resultat recupere par l'analyse
+      * @param str la chaine de caracteres recuperee par l'analyse
+      * @return la phrase qui contient le resultat de l'analyse
+      */
+    def construire(str: String):String = {
+        "L'adresse de " + resultat._1 + " est : " + resultat._2
+    }
 }
