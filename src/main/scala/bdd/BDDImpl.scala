5042ebe7baafb1a7f2c32772e8a31fa97d6ec5ed
@karzelguizio <kawrantin.arzel--guiziou@etudiant.univ-rennes1.fr>
2023-03-20 17:34:19 +0100

    indentation

--- src/main/scala/bdd/BDDImpl.scala
+++ src/main/scala/bdd/BDDImpl.scala
@@ -7,12 +7,11 @@ object BDDImpl extends BaseDeDonnees{
     val lines = Source.fromFile("doc/DonneesInitiales.txt").getLines.toArray
     val banwords = Array[String]("le","la","les","de","des","du","et","rennes","bretagne","ville")
     def chercherAdresse(str: String): String = {
-    
-    for (ligne <- lines){
-        if ((ligne.toLowerCase).contains(str.toLowerCase()) && !banwords.contains(str.toLowerCase())){
-            return ligne.split(";")(1)
+        for (ligne <- lines){
+            if ((ligne.toLowerCase).contains(str.toLowerCase()) && !banwords.contains(str.toLowerCase())){
+                return ligne.split(";")(1)
+            }
         }
-    }
         "Adresse non trouvée"
     }
 
