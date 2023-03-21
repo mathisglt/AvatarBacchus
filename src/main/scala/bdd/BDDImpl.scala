d0d2599093995fbe2ee38c6ef3fa1aa7ac5c6e0e
@karzelguizio <kawrantin.arzel--guiziou@etudiant.univ-rennes1.fr>
2023-03-21 09:52:56 +0100

    j'ai enlevé des banwords

--- src/main/scala/bdd/BDDImpl.scala
+++ src/main/scala/bdd/BDDImpl.scala
@@ -5,7 +5,7 @@ import scala.io.BufferedSource
 
 object BDDImpl extends BaseDeDonnees{
     val lines = Source.fromFile("doc/DonneesInitiales.txt").getLines.toArray
-    val banwords = Array[String]("le","la","les","de","des","du","et","rennes","bretagne","ville")
+    val banwords = Array[String]("le","la","les","de","des","du","et")
     def chercherAdresse(str: String): String = {
         for (ligne <- lines){
             if ((ligne.toLowerCase).contains(str.toLowerCase()) && !banwords.contains(str.toLowerCase())){
