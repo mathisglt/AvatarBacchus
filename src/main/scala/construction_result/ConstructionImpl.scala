500895d321bc2b63f7475c74d016909ab07303a3
evan <evan.froc@univ-rennes1.fr>
2023-03-21 10:14:00 +0100

    Construction valides f1

--- src/main/scala/construction_result/ConstructionImpl.scala
+++ src/main/scala/construction_result/ConstructionImpl.scala
@@ -2,21 +2,25 @@ package construction_result
 
 import analyse_phrase.AnalyseImpl
 
+
 object ConstructionImpl extends ConstructionTrait{
   
     val resultat = AnalyseImpl
 
     /**
       * Amélioration de la v0 qui prend plusieurs couples (lieu, adresse) et rend les phrases résultats
-      * @param resultAnalyse le resultat de l'analyse qui peut contenir plusieurs couples (lieu, adresse)
+      * @param requete requete (lieu, adresse)
       * @return la ou les phrases representant la reponse sous la forme d'une String
       */
-    def construirev1(resultAnalyse: List[(String, String)]):String = {
+    def construire(requete: String):String = {
         var phrase:String = ""
-        resultAnalyse match {
-            case Nil => ""
-            case (lieu, adresse)::reste => phrase += "L'adresse de " ++ lieu ++ " est : " ++ adresse ++ ". " ++ construirev1(reste)
-        }
+        var resultAnalyse = AnalyseImpl.analyser(requete)
+        //resultAnalyse match {
+        //    case Nil => ""
+           // case (lieu, adresse) => phrase += "L'adresse de " ++ lieu ++ " est : " ++ adresse ++ ". " ++ construirev1(reste)
+        //}
+        if (resultAnalyse != ("","")) phrase += "L'adresse de " ++ resultAnalyse._1 ++ " est : " ++ resultAnalyse._2 
+        else phrase = "Je ne comprends pas votre demande"
         phrase
     }
 }
--- src/main/scala/construction_result/ConstructionTrait.scala
+++ src/main/scala/construction_result/ConstructionTrait.scala
@@ -1,5 +1,10 @@
 package construction_result
 
 trait ConstructionTrait {
-  
+  /**
+      * Amélioration de la v0 qui prend plusieurs couples (lieu, adresse) et rend les phrases résultats
+      * @param resultAnalyse le resultat de l'analyse d'un couple (lieu, adresse)
+      * @return la phrases representant la reponse sous la forme d'une String
+      */
+    def construire(resultat: String):String 
 }
