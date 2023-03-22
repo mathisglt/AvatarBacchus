c3111da732baa0407702b72f335cfd8f9c5fa2cc
thild <thomas.hild@etudiant.univ-rennes1.fr>
2023-03-22 14:12:46 +0100

    creation test tolerance_fautes

---
+++ src/test/scala/tolerance_fautes/TestTolerance.scala
@@ -0,0 +1,27 @@
+package tolerance_fautes
+import tolerance_fautes.FautesImpl
+import junit.framework.Test
+
+class TestTolerance {
+    val listeTest:List[String]=
+    @Test
+  def Test1_Tolerance(): Unit ={
+    
+  }
+    @Test
+  def Test2_Tolerance(): Unit ={
+
+  }
+    @Test
+  def Test3_Tolerance(): Unit ={
+    
+  }
+    @Test
+  def Test4_Tolerance(): Unit ={
+    
+  }
+    @Test
+  def Test5_Tolerance(): Unit ={
+    
+  }
+}
