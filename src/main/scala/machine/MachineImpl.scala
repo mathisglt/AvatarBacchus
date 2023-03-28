fd0653ff3097a4514ea582c04ad29ea7698d1fbc
nolan <nolan.guiziou@etudiant.univ-rennes1.fr>
2023-03-28 10:34:00 +0200

    BDD v2

--- src/main/scala/machine/MachineImpl.scala
+++ src/main/scala/machine/MachineImpl.scala
@@ -6,12 +6,15 @@ object MachineImpl extends MachineDialogue {
 
   
   def ask(s: String): List[String] = {
-   List(ConstructionImpl.construire(s))
+    println(s)
+    List(ConstructionImpl.construire(s))
   }
   
   // Pour la partie test par le client
   def reinit(): Unit = ()
   def test(l: List[String]): List[String] = {
+    println(l)
+    println(l.map(x => ConstructionImpl.construire(x)))
     l.map(x => ConstructionImpl.construire(x))
   }
 }
