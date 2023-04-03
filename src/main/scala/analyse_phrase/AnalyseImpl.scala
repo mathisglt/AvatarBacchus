044e19bfcdcb7fb88768194e6537d1fd1b5e70ce
thild <thomas.hild@etudiant.univ-rennes1.fr>
2023-04-03 14:48:02 +0200

    légère correction scaladoc (result -> return)

--- src/main/scala/analyse_phrase/AnalyseImpl.scala
+++ src/main/scala/analyse_phrase/AnalyseImpl.scala
@@ -6,7 +6,6 @@ import bdd.BDDImpl
 import tolerance_fautes.FautesImpl
 
 case object ExceptionListeVide extends Exception
-//TODO scalaDoc
 object AnalyseImpl extends AnalyseTrait {
 
   val liste_lieux = BDDImpl
@@ -16,16 +15,16 @@ object AnalyseImpl extends AnalyseTrait {
 
   /** permet de retirer les mots de liaisons de phrase sous formes de liste de string
     *  @param phrase sous forme de liste de string
-    *  @result la phrase sous forme de liste de string sans les mots de liaisons
+    *  @return la phrase sous forme de liste de string sans les mots de liaisons
     */
   def filtreLiaison(listeLieu: List[String]): List[String] = {
     val liaisons = List("de", "La", "l")
     listeLieu.filter(listeLieu => !liaisons.contains(listeLieu))
   }
 
-  /** analyse une phrase sous forme de string en entrée, la corrige et y identifie un lieu que l'on connaît afin de nous renvoyer notre couple (lieu, adresse) 
+  /** analyse une phrase sous forme de string en entrée, la corrige et y identifie un lieu que l'on connaît afin de nous renvoyer notre couple (lieu, adresse)
     *  @param phrase qui est le string à analyser
-    *  @result un couple de string représentant le lieu ainsi quue son adresse (lieu,adresse)
+    *  @return un couple de string représentant le lieu ainsi quue son adresse (lieu,adresse)
     */
   def analyser(phrase: String): (String, String) = {
     val phrase_corrigee: String = assembler(
@@ -49,9 +48,9 @@ object AnalyseImpl extends AnalyseTrait {
     }
   }
 
-  /** decoupe un string en plusieurs string élémentts d'une liste de string 
-    *  @param phrase qui est le sstring à découper
-    *  @result une liste dont chaque élément est un mot de la phrase dans l'ordre dans lequel il apparaît dans le string de base 
+  /** decoupe un string en plusieurs string élémentts d'une liste de string
+    *  @param phrase qui est le string à découper
+    *  @return une liste dont chaque élément est un mot de la phrase dans l'ordre dans lequel il apparaît dans le string de base
     */
   def decouper(phrase: String): List[String] = phrase.split("[ .!?,;']+").toList
 
@@ -64,16 +63,16 @@ object AnalyseImpl extends AnalyseTrait {
 
   /** assemble une liste de mot (string) pour former une phrase sous forme de string avec un espace entre chaque mot
     *  @param list une list de mot
-    *  @result un string qui sera la phrase décrit par les éléments de départ
+    *  @return un string qui sera la phrase décrit par les éléments de départ
     */
   def assembler(list: List[String]): String = {
     if (list.isEmpty) throw ExceptionListeVide
     else list.reduce(_ + " " + _)
   }
 
-  /** détecte une formule de politesse dans une phrase(string)  
+  /** détecte une formule de politesse dans une phrase(string)
     *  @param phrase qui est le string à analyser
-    *  @result un booléen true si la phrase contient les mots bonjour bonsoir ou salut
+    *  @return un booléen true si la phrase contient les mots bonjour bonsoir ou salut
     */
   def politeTest_Bonjour(phrase: String): Boolean = {
     val phrase_corrigee: String = assembler(
