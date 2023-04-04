9cd1308fcc319abba84e356043487506fae25ba8
evan <evan.froc@univ-rennes1.fr>
2023-04-04 09:32:27 +0200

    construction f4v0

--- src/main/scala/construction_result/ConstructionImpl.scala
+++ src/main/scala/construction_result/ConstructionImpl.scala
@@ -4,24 +4,21 @@ import analyse_phrase.AnalyseImpl
 
 
 object ConstructionImpl extends ConstructionTrait{
-  
-    val resultat = AnalyseImpl
 
     /**
-      * Amélioration de la v0 qui prend plusieurs couples (lieu, adresse) et rend les phrases résultats
-      * @param requete requete (lieu, adresse)
-      * @return la ou les phrases representant la reponse sous la forme d'une String
+      * @param requete la requete de l'utilisateur
+      * @param langue la langue confirmé par l'utilisateur
+      * @return la phrase representant la reponse dans la langue voulu sous la forme d'une String
       */
-    def construire(requete: String):String = {
+    def construire(requete: String, langue: langue):String = {
         var phrase:String = ""
         var resultAnalyse = AnalyseImpl.analyser(requete)
         //resultAnalyse match {
         //    case Nil => ""
            // case (lieu, adresse) => phrase += "L'adresse de " ++ lieu ++ " est : " ++ adresse ++ ". " ++ construirev1(reste)
         //}
-        if (resultAnalyse != ("","")) phrase = "L'adresse de " ++ resultAnalyse._1 ++ " est : " ++ resultAnalyse._2 
-        else phrase = "Je ne comprends pas votre demande"
+        if (resultAnalyse != ("","")) phrase = expression(langue,2).replace("XXX",resultAnalyse._1) + resultAnalyse._2
+        else phrase = expression(langue, 3)
         phrase
-        
     }
 }
--- src/main/scala/construction_result/ConstructionTrait.scala
+++ src/main/scala/construction_result/ConstructionTrait.scala
@@ -2,10 +2,10 @@ package construction_result
 
 trait ConstructionTrait {
   /**
-      * Amélioration de la v0 qui prend plusieurs couples (lieu, adresse) et rend les phrases résultats
-      * @param resultAnalyse le resultat de l'analyse d'un couple (lieu, adresse)
-      * @return la phrases representant la reponse sous la forme d'une String
+      * @param requete la requete de l'utilisateur
+      * @param langue la langue confirmé par l'utilisateur
+      * @return la phrase representant la reponse dans la langue voulu sous la forme d'une String
       */
-    def construire(resultat: String):String 
+    def construire(requete: String, langue: langue):String 
 
 }
