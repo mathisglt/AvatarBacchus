e1d28e353af12037fab54d3588be151cc6bf1754
evanchr <95886771+evanchr@users.noreply.github.com>
2023-04-04 11:12:19 +0200

    réinitialisation de la langue

--- src/main/scala/machine/MachineImpl.scala
+++ src/main/scala/machine/MachineImpl.scala
@@ -2,23 +2,26 @@ package machine
 import construction_result.ConstructionImpl
 import analyse_phrase.AnalyseImpl
 import tolerance_fautes.FautesImpl
+import langue.LangueImpl
 
 object MachineImpl extends MachineDialogue {
 
-  
   def ask(s: String): List[String] = {
     println("\nrequete : " + s)
-    if (AnalyseImpl.politeTest_OnlyBonjour(s)) "Bonjour"::Nil
-    else if (AnalyseImpl.politeTest_Bonjour(s)) "Bonjour" :: List(ConstructionImpl.construire(s))
+    if (AnalyseImpl.politeTest_OnlyBonjour(s)) "Bonjour" :: Nil
+    else if (AnalyseImpl.politeTest_Bonjour(s))
+      "Bonjour" :: List(ConstructionImpl.construire(s))
     else List(ConstructionImpl.construire(s))
   }
-  
+
   // Pour la partie test par le client
-  def reinit(): Unit = ()
+  def reinit(): Unit = {
+    LangueImpl.setLangueActuelle("Français") //réinitialise la langue
+  }
   def test(l: List[String]): List[String] = {
     println("Requetes de base : " + l)
     l match {
-      case Nil => Nil
+      case Nil          => Nil
       case head :: next => ask(head) ++ test(next)
     }
   }
