fd60ad3ae2e2655214109d63ab71de4a933fa65b
@karzelguizio <kawrantin.arzel--guiziou@etudiant.univ-rennes1.fr>
2023-04-20 18:54:26 +0200

    petit détail que je voulais pas commit

--- src/main/scala/client/Client.scala
+++ src/main/scala/client/Client.scala
@@ -7,8 +7,5 @@ import automaticTester.TestAvatar
 import bdd.BDDImpl
 
 object Client extends App {
-  // TestAvatar.check(MachineImpl)
-  val list_bdd_xml = BDDImpl.createListFromXML()
-  println(list_bdd_xml)
-
+  TestAvatar.check(MachineImpl)
 }
