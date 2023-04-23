599cf7b6bf16c2eb2c18585b477b66c61a68c209
evanchr <95886771+evanchr@users.noreply.github.com>
2023-04-23 16:18:20 +0200

    MAJ AnalyseTrait

--- src/main/scala/analyse_phrase/AnalyseTrait.scala
+++ src/main/scala/analyse_phrase/AnalyseTrait.scala
@@ -9,20 +9,22 @@ trait AnalyseTrait {
   def analyser(phrase: String): (String,String)
 
   /**
-    * analyse la phrase du user et renvoie true s'il contient "bonjour", "bonsoir" ou "salut"
+    * analyse la phrase du user et renvoie true s'il contient "bonjour", "bonsoir" ou "salut", puis le bonjour à répondre
     *
     * @param phrase la requete du user
-    * @return true si contient un des mots, false sinon
+    * @return true si contient un des mots, false sinon ; 
+    * le string correspond au bonjour dans la langue correspondante que le robot doit répondre
     */
-  def politeTest_Bonjour(phrase: String): (Boolean,String)
+  def politeTest_Bonjour(phrase: String): (Boolean, String)
 
   /**
-    * analyse la phrase du user et renvoie true s'il contient uniquement "bonjour", "bonsoir" ou "salut"
+    * analyse la phrase du user et renvoie true s'il contient uniquement "bonjour", "bonsoir" ou "salut", puis le bonjour à répondre
     *
     * @param phrase la requete du user
-    * @return vrai si la phrase ne contient que "bonjour", "bonsoir" ou "salut" modulo erreurs et majuscules
+    * @return vrai si la phrase ne contient que "bonjour", "bonsoir" ou "salut" modulo erreurs et majuscules ; 
+    * le string correspond au bonjour dans la langue correspondante que le robot doit répondre
     */
-  def politeTest_OnlyBonjour(phrase: String): (Boolean,String)
+  def politeTest_OnlyBonjour(phrase: String): (Boolean, String)
   
   /**
     * Recupere le dictionnaire de la langue actuelle
@@ -32,10 +34,11 @@ trait AnalyseTrait {
   def getDicoLangue(): List[String]
 
   /**
-    * Detecte s'il y a changement de langue et donne l'int de la langue actuelle
+    * Detecte s'il y a une détection de langue de langue et donne l'int de celle-ci si c'est le cas et l'int de la langue actuelle sinon
     *
     * @param phrase
-    * @return le couple (boolean ; int) : (true si changement, false sinon ; int de la langue actuelle, qu'elle ait change ou non)
+    * @return le couple (boolean, int, int) : (true si détection, false sinon ; int de la langue détectée si c'est le cas et 
+    * langue actuelle sinon ; int de la langue actuelle pour comparer ensuite s'il y a un changement)
     */
   def detecLangue(phrase: String): (Boolean, Int, Int)
   
