bc78d3c84aa19b9b2888a85a9cac6e15d5af887a
evanchr <95886771+evanchr@users.noreply.github.com>
2023-04-23 16:46:32 +0200

    Mise au propre + ScalaDod de MachineImpl

--- src/main/scala/machine/MachineImpl.scala
+++ src/main/scala/machine/MachineImpl.scala
@@ -10,61 +10,70 @@ object MachineImpl extends MachineDialogue {
     println("requete : " + requete)
     val msgRobot = ConstructionImpl.construireLangue(requete) //renvoie le message du robot
 
-    if (!changementEnCours) { //s'il n'y a pas de changement en cours et que le message du robot est une des phrases suivantes, on change la langue
-      msgRobot.last match {
-        case "Parlez-vous français?" =>
-          LangueImpl.setLangueActuelle("Français"); changementEnCours = true;
-          msgRobot
-        case "Do you speak english?" =>
-          LangueImpl.setLangueActuelle("Anglais"); changementEnCours = true;
-          msgRobot
-        case "Hablas español?" =>
-          LangueImpl.setLangueActuelle("Espagnol"); changementEnCours = true;
-          msgRobot
-        case "Sprechen Sie Deutsch?" =>
-          LangueImpl.setLangueActuelle("Allemand"); changementEnCours = true;
-          msgRobot
-        case "Parli italiano?" =>
-          LangueImpl.setLangueActuelle("Italien"); changementEnCours = true;
-          msgRobot
-        case _ => msgRobot
+    if (!changementEnCours) {
+      //s'il n'y a pas de changement de langue en cours et que le message du robot est une question pour savoir la langue, 
+      //on change la langue et on active le changement
+      if (analyseMsgRobot(msgRobot).head != "Cas par défaut") {
+        changementEnCours = true
       }
       println("message robot : " + msgRobot)
-      msgRobot
+      msgRobot //on renvoie ensuite le message du robot dans tous les cas
+    } else {
+      //si un changement de langue est en cours on regarde si l'utilisateur à confirmé la langue
 
-    } else { //si un changement est en cours on regarde si l'utilisateur à confirmer la langue
-      val confirmation = ConstructionImpl.construireConfirmation(requete,LangueImpl.getLangueActuelle()) //on construit la phrase en fonction de la confirmation ou non
+      val confirmation = ConstructionImpl.construireConfirmation(requete,LangueImpl.getLangueActuelle()) 
+      //on construit la phrase en fonction de la confirmation ou non
+
       confirmation match {
-        //s'il n'y a pas de confirmation, on passe à la langue suivant et on demande à l'utilisateur si cela lui convient
+        //s'il n'y a pas de confirmation, on passe à la langue suivante et on demande à l'utilisateur si cela lui convient
         case "Pas de confirmation" =>
-          msgRobot.last match {
-            case "Parlez-vous français?" =>
-              LangueImpl.setLangueActuelle("Français");
-              msgRobot
-            case "Do you speak english?" =>
-              LangueImpl.setLangueActuelle("Anglais"); 
-              msgRobot
-            case "Hablas español?" =>
-              LangueImpl.setLangueActuelle("Espagnol");
-              msgRobot
-            case "Sprechen Sie Deutsch?" =>
-              LangueImpl.setLangueActuelle("Allemand");
-              msgRobot
-            case "Parli italiano?" =>
-              LangueImpl.setLangueActuelle("Italien");
-              msgRobot
-            case _ => LangueImpl.langueSuivante(); LangueImpl.getLangueQuestion() :: Nil
+          if (analyseMsgRobot(msgRobot).head == "Cas par défaut") {
+            LangueImpl.langueSuivante(); 
+            LangueImpl.getLangueQuestion() :: Nil
+          } else {
+            analyseMsgRobot(msgRobot)
           }
         //s'il y a confirmation, il n'y a plus de changement de langue en cours et on renvoie la confirmation que la langue à été changée
-        case _ => changementEnCours = false;println("message robot : " + confirmation); confirmation :: Nil
+        case _ => changementEnCours = false; 
+                  println("message robot : " + confirmation); 
+                  confirmation :: Nil
       }
+    }
+  }
+
+  /**
+    * Analyse si le message du robot est une question pour connaître la langue ou non
+    *
+    * @param msgRobot la liste de messages réponses du robot
+    * @return soit la réponse du robot après avoir changé la langue, 
+    *         soit "Cas par défaut" lorsque le message n'est pas une des questions
+    */
+  def analyseMsgRobot(msgRobot: List[String]): (List[String]) = {
+    var reponse = msgRobot
+    msgRobot.last match {
+      case "Parlez-vous français?" =>
+        LangueImpl.setLangueActuelle("Français"); 
+        reponse
+      case "Do you speak english?" =>
+        LangueImpl.setLangueActuelle("Anglais"); 
+        reponse
+      case "Hablas español?" =>
+        LangueImpl.setLangueActuelle("Espagnol"); 
+        reponse
+      case "Sprechen Sie Deutsch?" =>
+        LangueImpl.setLangueActuelle("Allemand"); 
+        reponse
+      case "Parli italiano?" => 
+        LangueImpl.setLangueActuelle("Italien"); 
+        reponse
+      case _                 => "Cas par défaut" :: Nil
     }
   }
 
   // Pour la partie test par le client
   def reinit(): Unit = {
     LangueImpl.reinitLangue //réinitialise la langue
-    changementEnCours = false
+    changementEnCours = false //réinitialise la changement de langue au cas où il y en avait un en cours
   }
   def test(l: List[String]): List[String] = {
     l match {
