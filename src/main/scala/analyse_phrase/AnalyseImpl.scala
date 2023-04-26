4de04bdd76736779d394c4b28e2cbda28285d80e
magaultier <mathis.gaultier@etudiant.univ-rennes1.fr>
2023-04-26 04:02:58 +0200

    6h pour seulement ça ... , F5 à 9/10

--- src/main/scala/analyse_phrase/AnalyseImpl.scala
+++ src/main/scala/analyse_phrase/AnalyseImpl.scala
@@ -16,13 +16,14 @@ object AnalyseImpl extends AnalyseTrait {
     .recupLieux(Source.fromFile("doc/DonneesInitiales.txt"))
     .concat(List("TNB", "Hotel de Ville"))
   val listeAvecLiaison = liste_lieux.map(decouper(_))
+  
 
   /** permet de retirer les mots de liaisons de phrase sous formes de liste de string
     *  @param requete sous forme de liste de string
     *  @return la phrase sous forme de liste de string sans les mots de liaisons
     */
   def filtreLiaison(requete: List[String]): List[String] = {
-    val liaisons = List("de", "des", "du", "le", "la", "les", "un", "une", "et", "l", "d")
+    val liaisons = List("de", "des", "du", "le", "la", "les", "un", "une", "et", "l", "d","où","je","i","ich")
     requete.filter(mot => !liaisons.contains(mot.toLowerCase()))
   }
 
@@ -38,32 +39,57 @@ object AnalyseImpl extends AnalyseTrait {
     assembler(decouper(requete).filter(mot => !dicoPolitesse.contains(mot.toLowerCase()) && !dicoRecherche.contains(mot.toLowerCase()) && !mot.toLowerCase().equals("rennes")))
   }
 
-  def analyser(phrase: String): (String, String) = {
-    val phrase_corrigee: String = assembler(
+ def analyser(phrase: String): (String, String) = {
+    // Les lieux de la base XML en version clean
+    val listlieuxxml = BDDImpl.xmlListLieu.map(x=>BDDImpl.removeLiaisonAccentsWords(BDDImpl.removeAccents(x.toLowerCase())))
+    // on enleve les "parasites" :
+    val phrase_sans_parasites: String = assembler(filtreLiaison(decouper(filtrePolitesseRecherche(phrase))))
+    // on corrige la phrase :
+      println("phrase : "+ phrase)
+    var phrase_corrigee: String = assembler(
       FautesImpl.correction(
-        decouper(phrase),
-        filtreLiaison(liste_lieux.flatMap(decouper(_)))
+        decouper(phrase_sans_parasites),
+        filtreLiaison(liste_lieux.flatMap(decouper(_)).concat(listlieuxxml))
       )
-    )
-    // Cas où l'on demande l'adresse directement , sans aucun mot supplémentaire
+    ).toLowerCase().replaceAll("pisicine","piscine").replaceAll("saint ","saint-") // TODO Corrections des piscines, replaceAll nécessaires pour test 49
+      println("phrase cor : "+ phrase_corrigee)
+    // On gère les exceptions , (équivalents), TNB ajouté pour qu'il puisse être instantanément detecté lorsqu'il est au milieu d'une phrase
+    val exceptions = List(("hotel ville","mairie"),("tnb","tnb"))
+    for (elem <- exceptions){
+      if (phrase_corrigee.contains(elem._1)){
+        return (BDDImpl.chercherLieu(elem._2), BDDImpl.chercherAdresse(elem._2))
+      }
+    }
+    //On retire les bonjours (et traductions) de la phrase
+    
+    if(politeTest_Bonjour(phrase_corrigee)._1){
+      val salutationsLangueActuelle = BDDImpl.createDicoSalutations(LangueImpl.langueIntToString(LangueImpl.getLangueActuelle()),LangueImpl.getLangueActuelle())
+      phrase_corrigee = assembler(FautesImpl.correction(decouper(phrase_corrigee),filtreLiaison(salutationsLangueActuelle)))
+      phrase_corrigee = removeBonjour(phrase_corrigee).replaceAll(" ","")
+    }
+    // Cas où l'on demande l'adresse directement, sans aucun mot supplémentaire :
     if(BDDImpl.chercherAdresse(phrase_corrigee) != "Adresse non trouvée"){
       return (BDDImpl.chercherLieu(phrase_corrigee), BDDImpl.chercherAdresse(phrase_corrigee))
     }
-    val mots = decouper(phrase_corrigee)
-    mots match {
-      case Nil => ("", "")
-      case head :: Nil =>
-        if (BDDImpl.chercherAdresse(head) != "Adresse non trouvée") {
-          (BDDImpl.chercherLieu(head), BDDImpl.chercherAdresse(head))
-        } else ("", "")
-      case head :: next => {
-        if (BDDImpl.chercherAdresse(head) != "Adresse non trouvée") {
-          (BDDImpl.chercherLieu(head), BDDImpl.chercherAdresse(head))
-        } else return analyser(next.mkString(" "))
+    // on recupere la liste des lieux :
+    val lieux = analyserList(decouper(BDDImpl.removeLiaisonAccentsWords(phrase_corrigee)))
+    for (lieu <- lieux) {
+      if(BDDImpl.removeLiaisonAccentsWords(lieu).contains(phrase_corrigee)){
+        (BDDImpl.chercherLieu(phrase_corrigee), BDDImpl.chercherAdresse(phrase_corrigee))
       }
     }
+    lieux match {
+      case Nil => ("", "")
+      case head :: Nil =>
+       
+        if (BDDImpl.chercherAdresse(BDDImpl.removeLiaisonAccentsWords(head)) != "Adresse non trouvée") {
+          (BDDImpl.chercherLieu(head), BDDImpl.chercherAdresse(head))
+        } else ("", "")
+      case head :: next => // TODO F6 il faudra gérer les cas avec plusieurs résultats 
+        val lieu_le_plus_courant = lieux.groupBy(identity).maxBy(_._2.length)._1
+        (lieu_le_plus_courant,BDDImpl.chercherAdresse(lieu_le_plus_courant))
+    }
   }
-
   /**
     * on cherche tous les lieux que cherche le user dans sa requete en regardant chaque mot un à un
     * attention : les filtres anti parasites sont déjà appliqués
@@ -72,12 +98,31 @@ object AnalyseImpl extends AnalyseTrait {
     * @return la liste des lieux
     */
   def analyserList(requete: List[String]): List[String] = {
-    requete match {
-      case Nil => Nil
-      case head :: next => quiContient(head, BDDImpl.xmlListLieu) ++ analyserList(next)
+    if (requete.length <= 1 ){
+      requete match {
+        case Nil => Nil
+        case head :: next => quiContient(head, BDDImpl.xmlListLieu)++ analyserList(next)
+      }
     }
+
+    else 
+      requete match {
+        case Nil => Nil
+        case head :: next => quiContient(head, BDDImpl.xmlListLieu)++ quiContient(head +"s", BDDImpl.xmlListLieu)++ analyserList(next)
+      }
   }
 
+  /**
+    * Retire de la phrase le mot bonjour et ses traductions
+    *
+    * @param phrase str représentant une phrase
+    * @return la phrase modifiée
+    */
+  def removeBonjour(phrase:String): String = {
+    val motsASupprimer = List("bonjour", "hello", "hola", "hallo", "buongiorno")
+    val regex = s"\\b(${motsASupprimer.mkString("|")})\\b"
+    phrase.replaceAll(regex, "")
+  }
   /** 
     * cherche tous les lieux de la bdd qui contiennent le mot passé en param
     * 
@@ -89,7 +134,7 @@ object AnalyseImpl extends AnalyseTrait {
     list match {
       case Nil => Nil
       case head :: next => 
-        val head_to_list = decouper(head.toLowerCase()) // on cherchera si le mot entier est un mot dans une adresse
+        val head_to_list = decouper(BDDImpl.removeLiaisonAccentsWords(BDDImpl.removeAccents(head.toLowerCase()))) // on cherchera si le mot entier est un mot dans une adresse
         if (head_to_list.contains(mot.toLowerCase())) head :: quiContient(mot, next)
         else quiContient(mot, next)
     }
@@ -118,7 +163,7 @@ object AnalyseImpl extends AnalyseTrait {
     *  @return un string qui sera la phrase décrit par les éléments de départ
     */
   def assembler(list: List[String]): String = {
-    if (list.isEmpty) throw ExceptionListeVide
+    if (list.isEmpty) return ""
     else list.reduce(_ + " " + _)
   }
 
