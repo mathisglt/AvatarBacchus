65663eb94cedb38fa8a65e355329e3588055cc19
thild <thomas.hild@etudiant.univ-rennes1.fr>
2023-04-29 19:09:02 +0200

    ajout file d'attente voix

--- src/main/scala/VoiceManagement/Voice.scala
+++ src/main/scala/VoiceManagement/Voice.scala
@@ -8,19 +8,51 @@ import marytts.exceptions.SynthesisException;
 import marytts.util.data.audio.AudioPlayer;
 import java.io.IOException
 import marytts.server.Mary
+import java.util.LinkedList
+import application.Application
 //les différentes voix : [istc-lucia-hsmm(italien), dfki-pavoque-neutral-hsmm(allemand), upmc-pierre-hsmm(francais), cmu-slt-hsmm(anglais)]
 object Voice extends VoiceTrait {
   private var interface: MaryInterface = new LocalMaryInterface();
   private var audioPlayer: AudioPlayer = new AudioPlayer();
+  private var fileAttente: LinkedList[(String, Int)] = new LinkedList()
 
+  def ajouteMessage(message: String, langue: Int): Unit = {
+    fileAttente.synchronized {
+      fileAttente.add((message, langue))
+      if (fileAttente.size() == 1) {
+        lire()
+      }
+    }
+  }
+
+  /** lis la file d'attente
+    */
+  def lire(): Unit = {
+    fileAttente.synchronized {
+      if (!fileAttente.isEmpty()) {
+        val message = fileAttente.getFirst()._1
+        val langue = fileAttente.getFirst()._2
+        fileAttente.removeFirst()
+        voice(langue)
+        say(message)
+        lire()
+      }
+    }
+  }
+
+  /** initialise la voix à français
+    */
   def InitVoix(): Unit = {
-    interface = new LocalMaryInterface()
     interface.setVoice("upmc-pierre-hsmm")
   }
 
+  /** change la langue en la langue demandée
+    *
+    * @param langue un entier correspondant à la langue voulue (l'espagnol n'est pas considéré)
+    * @return opère un changement sur la langue de parole
+    */
   def voice(langue: Int): Unit = {
     try {
-      interface = new LocalMaryInterface();
       langue match {
         case 0 => interface.setVoice("upmc-pierre-hsmm");
         case 1 => interface.setVoice("cmu-slt-hsmm");
@@ -35,15 +67,20 @@ object Voice extends VoiceTrait {
     }
   }
 
-  def say(phrase: String): Long = {
+  /** prononce la phrase en paramètre
+    *
+    * @param phrase une phrase sous forme de String
+    * @return le temps que l'interface met à dire la phrase et dit la phrase
+    */
+  def say(phrase: String): Unit = {
     try {
       val audio: AudioInputStream = interface.generateAudio(phrase);
 
       audioPlayer.setAudio(audio);
       audioPlayer.start();
-      return audio.getFrameLength()
     } catch {
-      case ex: SynthesisException => throw new Exception("impossible de dire la phrase")
+      case ex: SynthesisException =>
+        throw new Exception("impossible de dire la phrase")
     }
   }
 }
--- src/main/scala/VoiceManagement/VoiceTrait.scala
+++ src/main/scala/VoiceManagement/VoiceTrait.scala
@@ -2,21 +2,10 @@ package VoiceManagement
 
 trait VoiceTrait {
 
-  /** initialise la voix à français
-    */
-  def InitVoix(): Unit
-
-  /** change la langue en la langue demandée
+  /** ajoute un message dans la file d'attente et demarre la lecture s'il est le seul
     *
-    * @param langue un entier correspondant à la langue voulue (l'espagnol n'est pas considéré)
-    * @return opère un changement sur la langue de parole
+    * @param message un message a lire
+    * @param langue une langue
     */
-  def voice(langue: Int): Unit
-
-  /** prononce la phrase en paramètre
-    *
-    * @param phrase une phrase sous forme de String
-    * @return le temps que l'interface met à dire la phrase et dit la phrase
-    */
-  def say(phrase: String): Long
+  def ajouteMessage(message: String, langue: Int): Unit
 }
