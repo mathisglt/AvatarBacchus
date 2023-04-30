1392317eb623a09e9c3ac2ca607cfb1d90c26fdc
thild <thomas.hild@etudiant.univ-rennes1.fr>
2023-05-01 00:16:33 +0200

    enlèvement print de compréhension

--- src/main/scala/analyse_phrase/AnalyseImpl.scala
+++ src/main/scala/analyse_phrase/AnalyseImpl.scala
@@ -17,9 +17,13 @@ object AnalyseImpl extends AnalyseTrait {
     - sans doublons (.distinct)
     - en séparant les mots comme 'saint-georges' en 2 mots distincts 'saint' et 'georges'
     .flatten sert à transformer une List[List[String]] en List[String]
-  */
-  private val tmp = BDDImpl.xmlListLieu.map(l => AnalyseImpl.decouper(BDDImpl.removeAccents(l.toLowerCase()))).flatten.distinct
-  val liste_mots_bdd_xml = tmp.map(mot => mot.split("-").toList).flatten.distinct
+   */
+  private val tmp = BDDImpl.xmlListLieu
+    .map(l => AnalyseImpl.decouper(BDDImpl.removeAccents(l.toLowerCase())))
+    .flatten
+    .distinct
+  val liste_mots_bdd_xml =
+    tmp.map(mot => mot.split("-").toList).flatten.distinct
 
   // Fonctions d'analyse pour chercher une adresse
 
@@ -31,43 +35,73 @@ object AnalyseImpl extends AnalyseTrait {
     // on enleve les mots de recherche et de politesse :
     val sans_rech_poli = filtrePolitesseRecherche(sans_liaison)
     // a ce stade, on cherche "tnb" et "hotel ville" :
-    val exceptions = List(("hotel ville","mairie"),("tnb","tnb"))
-    var correction = assembler(FautesImpl.correction(decouper(sans_rech_poli),List("hotel","ville","tnb")))
-    for (elem <- exceptions){ 
-      if (correction.contains(elem._1)){
-        return List((BDDImpl.chercherLieu(elem._2), BDDImpl.chercherAdresse(elem._2)))
+    val exceptions = List(("hotel ville", "mairie"), ("tnb", "tnb"))
+    var correction = assembler(
+      FautesImpl.correction(
+        decouper(sans_rech_poli),
+        List("hotel", "ville", "tnb")
+      )
+    )
+    for (elem <- exceptions) {
+      if (correction.contains(elem._1)) {
+        return List(
+          (BDDImpl.chercherLieu(elem._2), BDDImpl.chercherAdresse(elem._2))
+        )
       }
     }
     val langue = LangueImpl.getLangueActuelle()
     val dicoExpr = BDDImpl.getDicoExpr()(langue)(8).split(",")
-    correction =  FautesImpl.correction(correction.split(" ").toList, dicoExpr.toList).mkString(" ")
-    if (correction.contains(dicoExpr(0)) || correction.contains(dicoExpr(1)) || correction.contains(dicoExpr(2))) {
+    correction = FautesImpl
+      .correction(correction.split(" ").toList, dicoExpr.toList)
+      .mkString(" ")
+    if (
+      correction.contains(dicoExpr(0)) || correction.contains(
+        dicoExpr(1)
+      ) || correction.contains(dicoExpr(2))
+    ) {
       var url = ""
       correction match {
         case _ if correction.contains(dicoExpr(0)) =>
           var restaurant = correction.split(dicoExpr(0))(1).replaceAll(" ", "+")
-          url = s"https://www.linternaute.com/restaurant/guide/ville-rennes-35000/?name=$restaurant"
+          url =
+            s"https://www.linternaute.com/restaurant/guide/ville-rennes-35000/?name=$restaurant"
         case _ if correction.contains(dicoExpr(1)) =>
-          val creperie = correction.split(dicoExpr(1))(1).trim.replaceAll(" ", "+")
-          url = s"https://www.linternaute.com/restaurant/guide/ville-rennes-35000/?name=$creperie"
-        case _ if correction.contains(dicoExpr(2))=>
-          val pizzeria = correction.split(dicoExpr(2))(1).trim.replaceAll(" ", "+")
-          url = s"https://www.linternaute.com/restaurant/guide/ville-rennes-35000/?name=$pizzeria"
+          val creperie =
+            correction.split(dicoExpr(1))(1).trim.replaceAll(" ", "+")
+          url =
+            s"https://www.linternaute.com/restaurant/guide/ville-rennes-35000/?name=$creperie"
+        case _ if correction.contains(dicoExpr(2)) =>
+          val pizzeria =
+            correction.split(dicoExpr(2))(1).trim.replaceAll(" ", "+")
+          url =
+            s"https://www.linternaute.com/restaurant/guide/ville-rennes-35000/?name=$pizzeria"
       }
-      val leHtml : Html = OutilsWebObjet.obtenirHtml(url)
+      val leHtml: Html = OutilsWebObjet.obtenirHtml(url)
       return List(getAdressFromHtml(leHtml))
     }
     // on selectionne des mots a corriger qui ne sont pas dans la bdd :
-    val mots_a_corriger = decouper(sans_rech_poli).filter(mot => !liste_mots_bdd_xml.contains(mot.toLowerCase()))
+    val mots_a_corriger = decouper(sans_rech_poli).filter(mot =>
+      !liste_mots_bdd_xml.contains(mot.toLowerCase())
+    )
     // on stocke les autres mots sans les corriger :
-    val mots_a_garder = decouper(sans_rech_poli).filter(mot => liste_mots_bdd_xml.contains(mot.toLowerCase()))
+    val mots_a_garder = decouper(sans_rech_poli).filter(mot =>
+      liste_mots_bdd_xml.contains(mot.toLowerCase())
+    )
     // on corrige ce qu'il faut corriger :
-    val liste_mots_corriges = FautesImpl.correction(mots_a_corriger, BDDImpl.recuplieuxBases ++ liste_mots_bdd_xml)
+    val liste_mots_corriges = FautesImpl.correction(
+      mots_a_corriger,
+      BDDImpl.recuplieuxBases ++ liste_mots_bdd_xml
+    )
     // on concatene les mots corrigés et gardés séparés par des espaces :
     val requete_corrigee = assembler(mots_a_garder ++ liste_mots_corriges)
     // Cas où l'on demande l'adresse directement, sans aucun mot supplémentaire :
-    if(BDDImpl.chercherAdresse(requete_corrigee) != "Adresse non trouvée") {
-      return List((BDDImpl.chercherLieu(requete_corrigee), BDDImpl.chercherAdresse(requete_corrigee)))
+    if (BDDImpl.chercherAdresse(requete_corrigee) != "Adresse non trouvée") {
+      return List(
+        (
+          BDDImpl.chercherLieu(requete_corrigee),
+          BDDImpl.chercherAdresse(requete_corrigee)
+        )
+      )
     }
     // on recupere la liste des lieux potentiels :
     val lieux = analyserList(decouper(requete_corrigee))
@@ -81,22 +115,24 @@ object AnalyseImpl extends AnalyseTrait {
       case head :: next =>
         val groupedLieux = lieux.groupBy(identity)
         val maxOccurences = groupedLieux.values.map(_.length).max
-        val lieux_les_plus_courants = groupedLieux.filter(_._2.length == maxOccurences).keys.toList.sorted
-        lieux_les_plus_courants.map{case lieu => (lieu,BDDImpl.chercherAdresse(lieu))}
+        val lieux_les_plus_courants =
+          groupedLieux.filter(_._2.length == maxOccurences).keys.toList.sorted
+        lieux_les_plus_courants.map { case lieu =>
+          (lieu, BDDImpl.chercherAdresse(lieu))
+        }
     }
   }
 
-  /**
-    * découpe un string en liste de mots
+  /** découpe un string en liste de mots
     *
     * @param phrase qui est le string à découper
     * @return une liste de string représentant la phrase decoupee
     */
-  def decouper(phrase: String): List[String] = phrase.split("[ .!?,;'()]+").toList
+  def decouper(phrase: String): List[String] =
+    phrase.split("[ .!?,;'()]+").toList
 
-  /** 
-    *  assemble une liste de mot (string) pour former une phrase sous forme de string avec un espace entre chaque mot
-    * 
+  /**  assemble une liste de mot (string) pour former une phrase sous forme de string avec un espace entre chaque mot
+    *
     *  @param list une list de mot
     *  @return un string qui sera la phrase décrit par les éléments de départ
     */
@@ -105,46 +141,69 @@ object AnalyseImpl extends AnalyseTrait {
     else list.reduce(_ + " " + _)
   }
 
-  /** 
-    * permet de retirer les mots de liaisons de phrase sous formes de liste de string
+  /** permet de retirer les mots de liaisons de phrase sous formes de liste de string
     * - on retire les mots ayant une longueur inf a 2
     * - on retire d'autres mots choisis
-    * 
+    *
     * @param requete sous forme de liste de string
     * @return la phrase sous forme de liste de string sans les mots de liaisons
     */
   def filtreLiaison(requete: List[String]): List[String] = {
-    val liaisons = List("se", "de", "des", "du", "d", "le", "la", "les", "l", "un", "une", "et", "je", "for")
-    requete.filter(mot => !liaisons.contains(mot.toLowerCase())).filter(_.length > 1)
+    val liaisons = List(
+      "se",
+      "de",
+      "des",
+      "du",
+      "d",
+      "le",
+      "la",
+      "les",
+      "l",
+      "un",
+      "une",
+      "et",
+      "je",
+      "for"
+    )
+    requete
+      .filter(mot => !liaisons.contains(mot.toLowerCase()))
+      .filter(_.length > 1)
   }
 
-  /**
-    * enleve de la requete du user tous les mots de Recherche ou de Politesse (on enleve "Rennes" aussi)
+  /** enleve de la requete du user tous les mots de Recherche ou de Politesse (on enleve "Rennes" aussi)
     *
     * @param requete la requete du user (String)
     * @return la requete sans les mots de Recherche ou de Politesse
     */
   def filtrePolitesseRecherche(requete: String): String = {
     val dicoUniversel = BDDImpl.getDicoPRN().flatten
-    val requete_corrigee = FautesImpl.correction(decouper(requete),dicoUniversel)
-    assembler(requete_corrigee.filter(mot => !dicoUniversel.contains(mot.toLowerCase()) && !mot.toLowerCase().equals("rennes")))
+    val requete_corrigee =
+      FautesImpl.correction(decouper(requete), dicoUniversel)
+    assembler(
+      requete_corrigee.filter(mot =>
+        !dicoUniversel
+          .contains(mot.toLowerCase()) && !mot.toLowerCase().equals("rennes")
+      )
+    )
   }
 
-  /**
-    * Retourne un couple (lieu , adresse) à partir d'un type Html
+  /** Retourne un couple (lieu , adresse) à partir d'un type Html
     *
     * @param html
     * @return (Lieu,Adresse)
     */
-  def getAdressFromHtml(html : Html) : (String,String) = {
-    val objFiltrageUrls:URLFiltres = new URLFiltres
+  def getAdressFromHtml(html: Html): (String, String) = {
+    val objFiltrageUrls: URLFiltres = new URLFiltres
     val cleanhtml = objFiltrageUrls.filtreAnnonce(html)
-    val htmldeladresse = OutilsWebObjet.obtenirHtml("https://www.linternaute.com/" + cleanhtml(0))
-    return (objFiltrageUrls.filtreAnnonceNom(htmldeladresse)(0),objFiltrageUrls.filtreAnnonceAdresse(htmldeladresse)(0))
+    val htmldeladresse =
+      OutilsWebObjet.obtenirHtml("https://www.linternaute.com/" + cleanhtml(0))
+    return (
+      objFiltrageUrls.filtreAnnonceNom(htmldeladresse)(0),
+      objFiltrageUrls.filtreAnnonceAdresse(htmldeladresse)(0)
+    )
   }
 
-  /**
-    * on cherche tous les lieux que cherche le user dans sa requete en regardant chaque mot un à un
+  /** on cherche tous les lieux que cherche le user dans sa requete en regardant chaque mot un à un
     * attention : les filtres anti parasites sont déjà appliqués
     *
     * @param requete la requete du user en List[String]
@@ -153,24 +212,28 @@ object AnalyseImpl extends AnalyseTrait {
   def analyserList(requete: List[String]): List[String] = {
     requete match {
       case Nil => Nil
-      case head :: next => quiContient(head, BDDImpl.xmlListLieu) ++ analyserList(next)
+      case head :: next =>
+        quiContient(head, BDDImpl.xmlListLieu) ++ analyserList(next)
     }
   }
 
-  /** 
-    * cherche tous les lieux de la bdd qui contiennent le mot passé en param
-    * 
+  /** cherche tous les lieux de la bdd qui contiennent le mot passé en param
+    *
     * @param mot le mot que l'on cherche dans la base de données
     * @param list, la liste des lieux de vAr.xml (à Rennes et ayant une adresse)
     * @return une liste de string représentant la liste des lieux contenant mot
     */
   def quiContient(mot: String, list: List[String]): List[String] = {
     list match {
-      case Nil => Nil
-      case head :: next => 
+      case Nil          => Nil
+      case head :: next =>
         // on cherchera si mot est bien un mot complet dans l'adresse :
         // (on prend la premiere adresse, on lui enleve les parasites, on le met en min, on remplace les "-" par des " ")
-        val head_to_list = decouper(BDDImpl.removeLiaisonAccentsWords(BDDImpl.removeAccents(head.toLowerCase()).replace("-"," ")))
+        val head_to_list = decouper(
+          BDDImpl.removeLiaisonAccentsWords(
+            BDDImpl.removeAccents(head.toLowerCase()).replace("-", " ")
+          )
+        )
         if (head_to_list.contains(mot)) head :: quiContient(mot, next)
         else quiContient(mot, next)
     }
@@ -178,26 +241,27 @@ object AnalyseImpl extends AnalyseTrait {
 
   def analyserChoix(requete: String): Option[Int] = {
     val requete_decoupee = decouper(requete)
-    print("requete_decoupee : " + requete_decoupee + " ; ")
-    val nombre_only = requete_decoupee.map(mot => mot.replaceAll("\\D","")).filter(_!="")
-    println("nombre_only : " + nombre_only)
+    val nombre_only =
+      requete_decoupee.map(mot => mot.replaceAll("\\D", "")).filter(_ != "")
     nombre_only match {
       case Nil => None
-      case head :: Nil => 
+      case head :: Nil =>
         try {
           Some(head.toInt)
-        }
-        catch {
+        } catch {
           case _: NumberFormatException => None
-        } 
+        }
       case head :: next => None
     }
   }
 
   // Fonctions d'analyse pour chercher de la politesse
 
-  def politeTest_Bonjour(phrase: String): (Boolean,String) = {
-    val salutationsLangueActuelle = BDDImpl.createDicoSalutations(LangueImpl.langueIntToString(LangueImpl.getLangueActuelle()),LangueImpl.getLangueActuelle())
+  def politeTest_Bonjour(phrase: String): (Boolean, String) = {
+    val salutationsLangueActuelle = BDDImpl.createDicoSalutations(
+      LangueImpl.langueIntToString(LangueImpl.getLangueActuelle()),
+      LangueImpl.getLangueActuelle()
+    )
     val phrase_corrigee: String = assembler(
       FautesImpl.correction(
         decouper(phrase),
@@ -206,15 +270,20 @@ object AnalyseImpl extends AnalyseTrait {
     )
     var testBonjour = false
     var i = 0
-    while (i < salutationsLangueActuelle.length && !testBonjour){
-      if(phrase_corrigee.toLowerCase().contains(salutationsLangueActuelle(i))) {testBonjour = true}
+    while (i < salutationsLangueActuelle.length && !testBonjour) {
+      if (
+        phrase_corrigee.toLowerCase().contains(salutationsLangueActuelle(i))
+      ) { testBonjour = true }
       i += 1
     }
-    (testBonjour,salutationsLangueActuelle(0))
+    (testBonjour, salutationsLangueActuelle(0))
   }
 
-  def politeTest_OnlyBonjour(phrase: String): (Boolean,String) = {
-    val salutationsLangueActuelle = BDDImpl.createDicoSalutations(LangueImpl.langueIntToString(LangueImpl.getLangueActuelle()),LangueImpl.getLangueActuelle())
+  def politeTest_OnlyBonjour(phrase: String): (Boolean, String) = {
+    val salutationsLangueActuelle = BDDImpl.createDicoSalutations(
+      LangueImpl.langueIntToString(LangueImpl.getLangueActuelle()),
+      LangueImpl.getLangueActuelle()
+    )
     val phrase_corrigee: String = assembler(
       FautesImpl.correction(
         decouper(phrase),
@@ -223,11 +292,13 @@ object AnalyseImpl extends AnalyseTrait {
     )
     var testOnlyBonjour = false
     var i = 0
-    while (i < salutationsLangueActuelle.length && !testOnlyBonjour){
-      if(phrase_corrigee.toLowerCase().equals(salutationsLangueActuelle(i))) {testOnlyBonjour = true}
+    while (i < salutationsLangueActuelle.length && !testOnlyBonjour) {
+      if (phrase_corrigee.toLowerCase().equals(salutationsLangueActuelle(i))) {
+        testOnlyBonjour = true
+      }
       i += 1
     }
-    (testOnlyBonjour,salutationsLangueActuelle(0))
+    (testOnlyBonjour, salutationsLangueActuelle(0))
   }
 
   // Fonctions d'analyse pour chercher la Langue
@@ -235,7 +306,9 @@ object AnalyseImpl extends AnalyseTrait {
   def getDicoLangue(): List[String] = {
     val dicoExpr = BDDImpl.getDicoExpr
     val langue_actuelle = LangueImpl.getLangueActuelle
-    dicoExpr(langue_actuelle).filter(_ != LangueImpl.langueIntToString(langue_actuelle))
+    dicoExpr(langue_actuelle).filter(
+      _ != LangueImpl.langueIntToString(langue_actuelle)
+    )
   }
 
   def getDicoLangue(lang: Int): List[String] = {
@@ -252,14 +325,23 @@ object AnalyseImpl extends AnalyseTrait {
   }
 
   private def detecLangue(phrase: List[String]): (Boolean, Int, Int) = {
-    var phrase_corrigee = FautesImpl.correction(filtreLiaison(phrase), List("français","english","español","deutsch","italiano"))
+    var phrase_corrigee = FautesImpl.correction(
+      filtreLiaison(phrase),
+      List("français", "english", "español", "deutsch", "italiano")
+    )
     phrase_corrigee match {
-      case Nil => (false, LangueImpl.getLangueActuelle(), LangueImpl.getLangueActuelle())
+      case Nil =>
+        (false, LangueImpl.getLangueActuelle(), LangueImpl.getLangueActuelle())
       case head :: next =>
         val langue = BDDImpl.langueDuMot(head)
         langue match {
           case "langue non détectée" => detecLangue(next)
-          case _ => (true, LangueImpl.langueStringToInt(langue), LangueImpl.getLangueActuelle())
+          case _ =>
+            (
+              true,
+              LangueImpl.langueStringToInt(langue),
+              LangueImpl.getLangueActuelle()
+            )
         }
     }
   }
--- src/main/scala/machine/MachineImpl.scala
+++ src/main/scala/machine/MachineImpl.scala
@@ -8,80 +8,83 @@ object MachineImpl extends MachineDialogue {
   var changementEnCours = false //changement de langue en cours
 
   def ask(requete: String): List[String] = {
-    println("requete : " + requete)
-    val msgRobot = ConstructionImpl.construireLangue(requete) //renvoie le message du robot
+    val msgRobot =
+      ConstructionImpl.construireLangue(requete) //renvoie le message du robot
 
     if (!changementEnCours) {
-      //s'il n'y a pas de changement de langue en cours et que le message du robot est une question pour savoir la langue, 
+      //s'il n'y a pas de changement de langue en cours et que le message du robot est une question pour savoir la langue,
       //on change la langue et on active le changement
       if (analyseMsgRobot(msgRobot).head != "Cas par défaut") {
         changementEnCours = true
       }
-      println("message robot : " + msgRobot)
       msgRobot //on renvoie ensuite le message du robot dans tous les cas
     } else {
       //si un changement de langue est en cours on regarde si l'utilisateur à confirmé la langue
 
-      val confirmation = ConstructionImpl.construireConfirmation(requete.replaceAll(" ",""),LangueImpl.getLangueActuelle()) 
+      val confirmation = ConstructionImpl.construireConfirmation(
+        requete.replaceAll(" ", ""),
+        LangueImpl.getLangueActuelle()
+      )
       //on construit la phrase en fonction de la confirmation ou non
 
       confirmation match {
         //s'il n'y a pas de confirmation, on passe à la langue suivante et on demande à l'utilisateur si cela lui convient
         case "Pas de confirmation" =>
-          if (analyseMsgRobot(msgRobot) == "Cas par défaut"::Nil) {
-            LangueImpl.langueSuivante(); 
+          if (analyseMsgRobot(msgRobot) == "Cas par défaut" :: Nil) {
+            LangueImpl.langueSuivante();
             LangueImpl.getLangueQuestion() :: Nil
           } else {
             analyseMsgRobot(msgRobot)
           }
         //s'il y a confirmation, il n'y a plus de changement de langue en cours et on renvoie la confirmation que la langue à été changée
-        case _ => changementEnCours = false; 
-                  println("message robot : " + confirmation); 
-                  confirmation :: Nil
+        case _ =>
+          changementEnCours = false;
+          confirmation :: Nil
       }
     }
   }
 
-  /**
-    * Analyse si le message du robot est une question pour connaître la langue ou non
+  /** Analyse si le message du robot est une question pour connaître la langue ou non
     *
     * @param msgRobot la liste de messages réponses du robot
-    * @return soit la réponse du robot après avoir changé la langue, 
+    * @return soit la réponse du robot après avoir changé la langue,
     *         soit "Cas par défaut" lorsque le message n'est pas une des questions
     */
   private def analyseMsgRobot(msgRobot: List[String]): (List[String]) = {
     msgRobot.last match {
       case "Parlez-vous français?" =>
-        LangueImpl.setLangueActuelle("Français"); 
+        LangueImpl.setLangueActuelle("Français");
         msgRobot
       case "Do you speak english?" =>
-        LangueImpl.setLangueActuelle("Anglais"); 
+        LangueImpl.setLangueActuelle("Anglais");
         msgRobot
       case "Hablas español?" =>
-        LangueImpl.setLangueActuelle("Espagnol"); 
+        LangueImpl.setLangueActuelle("Espagnol");
         msgRobot
       case "Sprechen Sie Deutsch?" =>
-        LangueImpl.setLangueActuelle("Allemand"); 
+        LangueImpl.setLangueActuelle("Allemand");
         msgRobot
-      case "Parli italiano?" => 
-        LangueImpl.setLangueActuelle("Italien"); 
+      case "Parli italiano?" =>
+        LangueImpl.setLangueActuelle("Italien");
         msgRobot
-      case _                 => "Cas par défaut" :: Nil
+      case _ => "Cas par défaut" :: Nil
     }
   }
 
-  def getLangueActuelle (): Int ={
+  def getLangueActuelle(): Int = {
     LangueImpl.getLangueActuelle()
   }
 
   // Pour la partie test par le client
   def reinit(): Unit = {
     LangueImpl.reinitLangue //réinitialise la langue
-    changementEnCours = false //réinitialise la changement de langue au cas où il y en avait un en cours
+    changementEnCours =
+      false //réinitialise la changement de langue au cas où il y en avait un en cours
     ConstructionImpl.choix_en_cours = false //réinitialise le choix du user
-    ConstructionImpl.liste_propositions_saved = Nil //réinitialise la liste des lieux proposés sauvegardée
+    ConstructionImpl.liste_propositions_saved =
+      Nil //réinitialise la liste des lieux proposés sauvegardée
   }
-  
+
   /** test de l'avatar
     *  @param l une liste de requête
     *  @return la liste des réponses produites par l'avatar
