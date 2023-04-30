da188c460add1c90c11d02cb6e7185d2b055cd58
evanchr <95886771+evanchr@users.noreply.github.com>
2023-04-30 17:06:45 +0200

    ScalaDoc et mise au propre du code package Fautes

--- src/main/scala/tolerance_fautes/FautesImpl.scala
+++ src/main/scala/tolerance_fautes/FautesImpl.scala
@@ -1,6 +1,8 @@
 package tolerance_fautes
+
 import scala.io.Source
 import org.apache.commons.lang3.StringUtils.{stripAccents}
+
 object FautesImpl extends FautesTrait {
 
   def correction(mots: List[String], modeles: List[String]): List[String] = {
@@ -16,12 +18,12 @@ object FautesImpl extends FautesTrait {
     result.reverse
   }
 
-    def correctionAvecPetitsMots(mots: List[String], modeles: List[String]): List[String] = {
+  def correctionAvecPetitsMots(mots: List[String], modeles: List[String]): List[String] = {
     var result: List[String] = Nil
     val modelesCleared = clearAccentToMaj(modeles)
     val motsTestsCleared = clearAccentToMaj(mots)
     for (incr <- 0 to motsTestsCleared.length - 1) {
-      testChaqueMotAvecPetitsMots(motsTestsCleared(incr), modelesCleared) match {
+      testChaqueMotAvecPetitsMots(motsTestsCleared(incr),modelesCleared) match {
         case -1  => result = mots(incr) :: result
         case num => result = modeles(num) :: result
       }
@@ -37,24 +39,32 @@ object FautesImpl extends FautesTrait {
     * @return l'emplacement du mot le plus proche dans la liste ou -1 s'il n'y en a pas (si c'est un petit mot on ne regarde plus)
     */
   def testChaqueMot(motATester: String, modeles: List[String]): Int = {
-    if(motATester.length<=2){return -1}
+    if (motATester.length <= 2) { return -1 }
     else {
-      val test = modeles.indexWhere((modele)=>modele==motATester)
-      if (test!=(-1)){test}else{modeles.indexWhere((modele) => distanceDeHammingInf1(motATester, modele))}
+      val test = modeles.indexWhere((modele) => modele == motATester)
+      if (test != (-1)) { test }
+      else {
+        modeles.indexWhere((modele) => distanceDeHammingInf1(motATester, modele))
+      }
     }
   }
 
-    /** prends un mot et une liste de modele et renvoie
+  /** prends un mot et une liste de modele et renvoie
     * son emplacement dans la liste modele ou -1 si il n'y figure pas
     *
     * @param motATester une String qu'on veut tester
     * @param modeles une liste des modeles
-    * @return l'emplacement du mot le plus proche dans la liste ou -1 s'il n'y en a pas 
+    * @return l'emplacement du mot le plus proche dans la liste ou -1 s'il n'y en a pas
     */
   def testChaqueMotAvecPetitsMots(motATester: String, modeles: List[String]): Int = {
-    val test = modeles.indexWhere((modele)=>modele==motATester)
-      if (test!=(-1)){test}else{modeles.indexWhere((modele) => distanceDeHammingInf1(motATester, modele))}
+    val test = modeles.indexWhere((modele) => modele == motATester)
+    if (test != (-1)) { 
+      test 
+    } else {
+      modeles.indexWhere((modele) => distanceDeHammingInf1(motATester, modele))
+    }
   }
+
   /** regarde si la distance de Hamming entre deux strings est supérieure ou égal à 1
     * si on voit que la chaine a tester est plus petite que la chaine modele on regarde si c'est du a un decalage
     * si on voit qu'elles sont de meme taille on regarde si il n'y a bien qu'une faute max
@@ -64,7 +74,9 @@ object FautesImpl extends FautesTrait {
     * @return true si la string test est à 1 de distance maximum de la string modele
     */
   def distanceDeHammingInf1(strtest: String, strmodele: String): Boolean = {
-    (strmodele.length - strtest.length == 0 && test1FauteMax(strtest,strmodele)) || (strmodele.length - strtest.length == 1 && testDecalage(strtest, strmodele))||(strtest.length-strmodele.length==1&&testDecalage(strmodele,strtest))
+    (strmodele.length - strtest.length == 0 && test1FauteMax(strtest,strmodele)) || 
+    (strmodele.length - strtest.length == 1 && testDecalage(strtest,strmodele)) || 
+    (strtest.length - strmodele.length == 1 && testDecalage(strmodele,strtest))
   }
 
   /** prends deux string de meme taille et renvoie si elles sont décalées de 1 (en gros si il y a une lettre erronée)
@@ -75,10 +87,7 @@ object FautesImpl extends FautesTrait {
     */
   def test1FauteMax(strtest: String, strmodele: String): Boolean = {
     strtest.isEmpty() ||
-    (strtest(0) == strmodele(0) && test1FauteMax(
-      strtest.tail,
-      strmodele.tail
-    )) ||
+    (strtest(0) == strmodele(0) && test1FauteMax(strtest.tail,strmodele.tail)) ||
     strtest.tail == strmodele.tail
   }
 
@@ -90,10 +99,7 @@ object FautesImpl extends FautesTrait {
     */
   def testDecalage(strtest: String, strmodele: String): Boolean = {
     strtest.isEmpty() ||
-    (strtest(0) == strmodele(0) && testDecalage(
-      strtest.tail,
-      strmodele.tail
-    )) ||
+    (strtest(0) == strmodele(0) && testDecalage(strtest.tail,strmodele.tail)) ||
     strtest == strmodele.tail
   }
 
@@ -104,8 +110,7 @@ object FautesImpl extends FautesTrait {
     */
   def clearAccentToMaj(mots: List[String]): List[String] = {
     mots match {
-      case head :: next =>
-        stripAccents(head).toUpperCase :: clearAccentToMaj(next)
+      case head :: next => stripAccents(head).toUpperCase :: clearAccentToMaj(next)
       case Nil => Nil
     }
   }
--- src/main/scala/tolerance_fautes/FautesTrait.scala
+++ src/main/scala/tolerance_fautes/FautesTrait.scala
@@ -1,17 +1,20 @@
 package tolerance_fautes
 
 trait FautesTrait {
-  //Sur le principe le but va être d'extraire les mots de la base de données et les mettre sous forme de liste,
-  //leur enlever leurs accents, puis les mettre en majuscule
-  //ainsi on obtient la liste des mots de la base de données à vérifier filtrée de ses majuscules et de ses accents,
-  //On fait de même avec la liste de mots qui est envoyée ici (la liste des mots de la phrase à analyser)
-  //puis on les compare avec la distance de levenstein
-  //On aura donc :
-  // -une fonction qui prend une liste de chaines de caracteres et en enleve les accents puis les mets en majuscules(potentiellement par deux fonctions auxiliaires)
-  // -une fonction qui compare deux mots avec levenstein
+  /*
+  Sur le principe le but va être d'extraire les mots de la base de données et les mettre sous forme de liste,
+  leur enlever leurs accents, puis les mettre en majuscule
+  ainsi on obtient la liste des mots de la base de données à vérifier filtrée de ses majuscules et de ses accents,
+  On fait de même avec la liste de mots qui est envoyée ici (la liste des mots de la phrase à analyser)
+  puis on les compare avec la distance de levenstein
+  On aura donc :
+   -une fonction qui prend une liste de chaines de caracteres et en enleve les accents puis les mets en majuscules(potentiellement par deux fonctions auxiliaires)
+   -une fonction qui compare deux mots avec levenstein
+  */
 
   //ici la liste de mots 'modele' en quelque sorte
   //on ne regarde pas les petits mots (<3)
+
   /** applique testChaqueMot sur toute une liste de mot et en renvoie la string corrigée
     *
     * @param mots une liste de mots
@@ -19,6 +22,7 @@ trait FautesTrait {
     */
 
   def correction(mots: List[String], modeles: List[String]): List[String]
+
   //meme fonction que la precedente mais on corrige les petits mots
     /** applique testChaqueMotAvecPetitsMots sur toute une liste de mot et en renvoie la string corrigée
     *
