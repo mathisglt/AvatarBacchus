e68c0e0cf07f39cd1b5136842fd70e2dd9043600
evanchr <95886771+evanchr@users.noreply.github.com>
2023-05-01 13:49:24 +0200

    Package construction_result : ScalaDoc, mise au propre du code et Trait

--- src/main/scala/construction_result/ConstructionTrait.scala
+++ src/main/scala/construction_result/ConstructionTrait.scala
@@ -2,20 +2,22 @@ package construction_result
 
 trait ConstructionTrait {
 
-/**
-  * check les bonjours et repond le cas echéant
-  *
-  * @param requete la requete de l'utilisateur
-  * @return un bonjour si bonjour + construire
-  */
-  def construirePolitesse(requete: String):List[String]
-  
-  /**
-    * construit la phrase de reponse pour l'utilisateur
-    * 
-    * @param requete la requete de l'utilisateur
-    * @return la phrase representant la reponse dans la langue voulu sous la forme d'une String
+  /** Si une langue est détectée, on demande à l'utilisateur s'il parle cette langue,
+    * sinon on renvoie les réponses du robot correspondantes à la requête 
+    *
+    * @param requete, la requête de l'utilisateur
+    * @return soit, la question pour demander à l'utilisateur s'il parle la langue détectée
+    * soit, les réponses du robot à la requête
     */
-    def construireReponseUnique(requete: String):String 
+  def construireLangue(requete: String): List[String]
+
+  /** Renvoie la confirmation à l'utilisateur que la langue à bien été changée 
+    * si sa requête était "oui" dans la même langue, et renvoie "Pas de confirmation" sinon 
+    *
+    * @param requete la requête de l'utilisateur (soit un "oui" dans la bonne langue soit autre chose)
+    * @param langueActuelle la langue actuelle
+    * @return la confirmation dans la bonne langue
+    */
+  def construireConfirmation(requete: String, langueActuelle: Int): String
 
 }
