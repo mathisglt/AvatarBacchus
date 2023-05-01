9425ae3ef92fa75ea59a104b458e37e5d1da066f
evanchr <95886771+evanchr@users.noreply.github.com>
2023-05-01 12:17:02 +0200

    Mise au propre du code package analyse_html

--- src/main/scala/analyse_html/FiltreHTML .scala
+++ src/main/scala/analyse_html/FiltreHTML .scala
@@ -1,22 +1,22 @@
-//Ca marche 
-package application
+package analyse_html
+
 import library.FiltrageHtml
 import library.Html
 import library.Expression
 import library.Et
 import library.Ou
 import library.Mot
-
-import application.HtmlToString
+import analyse_html.HtmlToString
 import library.Tag
 import library.Texte
 import library.HtmlExample
+
 class FiltreHTML extends FiltrageHtml{
- /**
-   * @param h un document Html
-   * @param e une expression
-   * @return si le document satisfait l'expression
-   */
+  /**
+    * @param h un document Html
+    * @param e une expression
+    * @return si le document satisfait l'expression
+    */
   def filtreHtml(h:Html,e:Expression):Boolean = {
     e match {
       case Et(e1, e2) => filtreHtml(h,e1) && filtreHtml(h,e2)
@@ -30,25 +30,26 @@ class FiltreHTML extends FiltrageHtml{
     * @param mot un mot
     * @return si le mot est contenu dans le texte du document
     */
-    def estContenu(h:Html,mot:String):Boolean={
-      h match {
-        case Tag(_, _, children) => estContenuEnfants(children,mot)
-        case Texte(content) => return content.contains(mot)
-      }
+  def estContenu(h:Html,mot:String):Boolean={
+    h match {
+      case Tag(_, _, children) => estContenuEnfants(children,mot)
+      case Texte(content) => return content.contains(mot)
     }
-    /**
-      * @param hs une liste d'objets html
-      * @param mot une string
-      * @return si le mot se trouve dans une des pages html
-      */
-    def estContenuEnfants(hs:List[Html],mot:String):Boolean={
-      for (h<-hs){
-        if (estContenu(h,mot)) return true
-      }
-      return false
+  }
+  
+  /**
+    * @param hs une liste d'objets html
+    * @param mot une string
+    * @return si le mot se trouve dans une des pages html
+    */
+  def estContenuEnfants(hs:List[Html],mot:String):Boolean={
+    for (h<-hs){
+      if (estContenu(h,mot)) return true
     }
-
+    return false
+  }
 }
+
 object testFiltreHtml extends App{//batterie de test de filtreHTML parce que mon vs code a des problemes pour run les tests 
   val obj:FiltreHTML=new FiltreHTML
   println(obj.filtreHtml(HtmlExample.exemple,Mot("Lien")))//true
